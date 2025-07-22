73d8d4964012dac83ac65c9c450ef1b652367881
robert+jan@gcash.com <robert+jan@gcash.com>
2025-07-22 10:52:50 +0800

    Final Submission for Lab1.5 (robert+jan@gcash.com)

--- lab1.5/src/test/java/com/tddacademy/zoo/controller/ZooControllerTest.java
+++ lab1.5/src/test/java/com/tddacademy/zoo/controller/ZooControllerTest.java
@@ -48,10 +48,13 @@ class ZooControllerTest {
         //    - jsonPath("$.description").value("A beautiful zoo in the heart of Manila")
         
         // Your code here:
-        // mockMvc.perform(get("/api/zoos/1"))
-        //     .andExpect(...)
-        //     .andExpect(...)
-        //     .andExpect(...);
+         mockMvc.perform(get("/api/zoos/1"))
+                 .andExpect(status().isOk())
+                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                 .andExpect(jsonPath("$.id").value(1))
+                 .andExpect(jsonPath("$.name").value("Manila Zoo"))
+                 .andExpect(jsonPath("$.location").value("Manila, Philippines"))
+                 .andExpect(jsonPath("$.description").value("A beautiful zoo in the heart of Manila"));
     }
 
     @Test
@@ -62,7 +65,7 @@ class ZooControllerTest {
         // 2. Add expectation for status().isNotFound()
         
         // Your code here:
-        // mockMvc.perform(get("/api/zoos/999"))
-        //     .andExpect(...);
+         mockMvc.perform(get("/api/zoos/999"))
+             .andExpect(status().isNotFound());
     }
 } 
